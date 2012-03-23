4788d504c8a142e3621753e5bf371c1d3a6a3e4a
zirrok80@gmail.com <zirrok80@gmail.com@736dbd85-bfc5-7efd-ea6b-cb364efeccb5>
2012-03-23 08:35:22 +0000

    projekt in den trunk verschoben

---
+++ de.bjoern.openworkspacefile/src/de/bjoern/openworkspacefile/util/OpenWorkspaceFileHelper.java
@@ -0,0 +1,314 @@
+package de.bjoern.openworkspacefile.util;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.ide.IDE;
+
+import de.bjoern.openworkspacefile.Activator;
+
+/**
+ * Helper class providing methods to find resources and open editors.
+ * 
+ * @author funhoff
+ */
+public class OpenWorkspaceFileHelper {
+
+	/**
+	 * Path separator.
+	 */
+	private static final String PATH_SEPARATOR = "/";
+
+	/**
+	 * Private constructor.
+	 * 
+	 * @since Creation date: 22.03.2012
+	 */
+	private OpenWorkspaceFileHelper() {
+		// prevent instantiation
+	}
+
+	/**
+	 * Opens the the editor with the given id and resource.
+	 * 
+	 * @param activePage
+	 *            The page in which the editor will be opened.
+	 * @param file
+	 *            The file to open.
+	 * @since Creation date: 14.03.2012
+	 */
+	public static void openEditor(final IWorkbenchPage activePage, final IFile file) {
+
+		Display.getDefault().syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					IDE.openEditor(activePage, file);
+					/*
+					 * Open Type command id:
+					 * org.eclipse.jdt.ui.navigate.open.type Open Resource
+					 * command id: org.eclipse.ui.navigate.openResource
+					 */
+				}
+
+				catch (PartInitException e) {
+					showAndLogErrorMessage("The editor could not be openend.", e);
+				}
+			}
+		});
+
+	}
+
+	/**
+	 * Shows and logs an error message with the given text to the user.
+	 * 
+	 * @param message
+	 *            The error message text to show.
+	 * @param trowable
+	 *            Throwable to log. Can be <code>null</code>.
+	 * @since Creation date: 13.03.2012
+	 */
+	public static void showAndLogErrorMessage(final String message, Throwable trowable) {
+		Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, message, trowable));
+		Display.getDefault().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				MessageDialog.openError(Display.getDefault().getActiveShell(), "An error occured during opening the workspace file", message);
+			}
+		});
+	}
+
+	/**
+	 * Returns the corresponding workspace file of the given repository URI. Can
+	 * be <code>null</code> if file not found.
+	 * 
+	 * @param repositoryURI
+	 *            The URI of the repository source.
+	 * @return The corresponding workspace file if found, otherwise returns
+	 *         <code>null</code>.
+	 * @since Creation date: 14.03.2012
+	 */
+	public static IFile getWorkspaceFile(String repositoryURI) {
+		String uri = "";
+		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+		String[] split = repositoryURI.split(PATH_SEPARATOR);
+		for (int i = split.length - 1; i >= 0; i--) {
+			if (i == split.length - 1) {
+				uri = split[i] + uri;
+			}
+			else {
+				uri = split[i] + PATH_SEPARATOR + uri;
+			}
+			Path path = new Path(uri);
+
+			if (workspaceRoot.exists(path)) {
+				IFile fileForLocation = workspaceRoot.getFile(path);
+				IFile[] filesForLocationURI = workspaceRoot.findFilesForLocationURI(fileForLocation.getLocationURI());
+				return getNotResourceFile(filesForLocationURI);
+			}
+		}
+		return getWorkspaceFileFallBackStrategy(split[split.length - 1], repositoryURI);
+	}
+
+	/**
+	 * Returns on of the given files. Should not be the resource file.
+	 * 
+	 * @param files
+	 *            Files to choose the 'right' on.
+	 * @return The {@link IFile} not representing the resource file.
+	 * @since Creation date: 22.03.2012
+	 */
+	private static IFile getNotResourceFile(IFile[] files) {
+		Map<IFile, Integer> fileSeparatorCountMap = new HashMap<IFile, Integer>();
+		if (files.length == 1) {
+			return files[0];
+		}
+		else {
+			for (int i = 0; i < files.length; i++) {
+				String[] filePathSplit = files[i].getFullPath().toString().split("/");
+				int separatorCount = filePathSplit.length;
+				fileSeparatorCountMap.put(files[i], separatorCount);
+			}
+			Set<IFile> keySet = fileSeparatorCountMap.keySet();
+			IFile fileToReturn = null;
+			int lowestCount = Integer.MAX_VALUE;
+			for (Iterator<IFile> iterator = keySet.iterator(); iterator.hasNext();) {
+				IFile file = iterator.next();
+				int separatorCount = fileSeparatorCountMap.get(file);
+				if (separatorCount < lowestCount) {
+					fileToReturn = file;
+					lowestCount = separatorCount;
+				}
+			}
+			return fileToReturn;
+		}
+	}
+
+	/**
+	 * Searches the workspace for the most likely file.
+	 * 
+	 * @param filename
+	 *            The file name to search.
+	 * @param repositoryURI
+	 *            The URI of the repository source.
+	 * @return The corresponding workspace file if found, otherwise returns
+	 *         <code>null</code>.
+	 */
+	private static IFile getWorkspaceFileFallBackStrategy(String filename, String repositoryURI) {
+		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+		try {
+			IResource[] members = workspaceRoot.members();
+			IFile[] filesFromResources = getWorkspaceFileFromResources(members, filename);
+			if (filesFromResources != null && filesFromResources.length > 0) {
+				if (filesFromResources.length == 1) {
+					return filesFromResources[0];
+				}
+				else {
+					return getMostLikelyFile(filesFromResources, repositoryURI);
+				}
+			}
+		}
+		catch (CoreException e) {
+			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, "The fall back strategy failed", e));
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the depending {@link IFile}s if the given filename is found in
+	 * one of the given {@link IResource}s.
+	 * 
+	 * @param resources
+	 *            The resources to search in.
+	 * @param filename
+	 *            The searched file name.
+	 * @return The corresponding {@link IFile}s if found. Otherwise
+	 *         <code>null</code> is returned.
+	 * @throws CoreException
+	 */
+	private static IFile[] getWorkspaceFileFromResources(IResource[] resources, String filename) throws CoreException {
+		Vector<IFile> foundFilesVector = new Vector<IFile>();
+		for (int i = 0; i < resources.length; i++) {
+			IFile[] files = getWorkspaceFileFromSingleResource(resources[i], filename);
+			if (files != null && files.length > 0) {
+				for (int j = 0; j < files.length; j++) {
+					foundFilesVector.add(files[j]);
+				}
+			}
+		}
+		if (!foundFilesVector.isEmpty()) {
+			return foundFilesVector.toArray(new IFile[foundFilesVector.size()]);
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the depending {@link IFile} if the given filename is found in the
+	 * given {@link IResource}.
+	 * 
+	 * @param resource
+	 *            The resource to search in.
+	 * @param filename
+	 *            The searched file name.
+	 * @return The corresponding {@link IFile} if found. Otherwise
+	 *         <code>null</code> is returned.
+	 * @throws CoreException
+	 */
+	private static IFile[] getWorkspaceFileFromSingleResource(IResource resource, String filename) throws CoreException {
+		if (resource instanceof IFile) {
+			IFile file = (IFile) resource;
+			if (filename.equals(file.getName())) {
+				return new IFile[]{file};
+			}
+		}
+		else if (resource instanceof IFolder) {
+			IFolder folder = (IFolder) resource;
+			IResource[] members = folder.members();
+			if (members.length > 0) {
+				return getWorkspaceFileFromResources(members, filename);
+			}
+		}
+		else if (resource instanceof IProject) {
+			IProject project = (IProject) resource;
+			if (project.isOpen()) {
+				IResource[] members = project.members();
+				if (members.length > 0) {
+					return getWorkspaceFileFromResources(members, filename);
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the most likely searched file.
+	 * 
+	 * @param files
+	 *            The file to choose the most likely one.
+	 * @param repositoryURI
+	 *            The URI of the repository source.
+	 * @return The most likely searched file.
+	 */
+	private static IFile getMostLikelyFile(IFile[] files, String repositoryURI) {
+		Map<IFile, Integer> filePathIdentitiesMap = new HashMap<IFile, Integer>();
+		for (int i = 0; i < files.length; i++) {
+			filePathIdentitiesMap.put(files[i], 0);
+			String[] repositorySplit = repositoryURI.split(PATH_SEPARATOR);
+			String[] foundFileSplit = files[i].getFullPath().toString().split(PATH_SEPARATOR);
+			int inspectDeep = (repositorySplit.length > foundFileSplit.length) ? foundFileSplit.length : repositorySplit.length;
+			for (int j = 0; j < inspectDeep; j++) {
+				if (repositorySplit[repositorySplit.length - 1 - j].equals(foundFileSplit[foundFileSplit.length - 1 - j])) {
+					int oldDeep = filePathIdentitiesMap.get(files[i]);
+					filePathIdentitiesMap.put(files[i], ++oldDeep);
+				}
+				else {
+					break;
+				}
+			}
+		}
+
+		return evaluateMostLikelyFile(filePathIdentitiesMap);
+	}
+
+	/**
+	 * Returns the file with the highest path identity deep in the given map.
+	 * 
+	 * @param filePathIdentitiesMap
+	 *            The Map containing file and path identity deep.
+	 * @return The {@link IFile} with the highest path identity deep.
+	 */
+	private static IFile evaluateMostLikelyFile(Map<IFile, Integer> filePathIdentitiesMap) {
+		IFile mostLikelyFile = null;
+		int mostLikelyFileDeep = 0;
+		Set<IFile> keySet = filePathIdentitiesMap.keySet();
+		for (Iterator<IFile> iterator = keySet.iterator(); iterator.hasNext();) {
+			IFile file = iterator.next();
+			int deep = filePathIdentitiesMap.get(file);
+			if (deep > mostLikelyFileDeep) {
+				mostLikelyFileDeep = deep;
+				mostLikelyFile = file;
+			}
+		}
+		return mostLikelyFile;
+	}
+
+}
---
+++ de.bjoern.openworkspacefile/src/de/bjoern/openworkspacefile/util/SearchAndOpenFileInWorkspaceJob.java
@@ -0,0 +1,96 @@
+package de.bjoern.openworkspacefile.util;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.ui.IWorkbenchPage;
+
+/**
+ * Job to search for and open a file.
+ * 
+ * @author funhoff
+ */
+public class SearchAndOpenFileInWorkspaceJob extends Job {
+
+	/**
+	 * The active page.
+	 */
+	private IWorkbenchPage activePage;
+
+	/**
+	 * Path to a file, e.g. a repository URL path.
+	 */
+	private String filePath = null;
+
+	/**
+	 * File to open in editor.
+	 */
+	private IFile file = null;
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param activePage
+	 *            The active page.
+	 * @param filePath
+	 *            The path to a file.
+	 * @since Creation date: 22.03.2012
+	 */
+	public SearchAndOpenFileInWorkspaceJob(IWorkbenchPage activePage, String filePath) {
+		super("Searching for corresponding file in workspace.");
+		this.activePage = activePage;
+		this.filePath = filePath;
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param activePage
+	 *            The active page.
+	 * @param file
+	 *            The file to open.
+	 * @since Creation date: 22.03.2012
+	 */
+	public SearchAndOpenFileInWorkspaceJob(IWorkbenchPage activePage, IFile file) {
+		this(activePage, "");
+		this.file = file;
+	}
+
+	@Override
+	protected IStatus run(IProgressMonitor monitor) {
+		if (!filePath.isEmpty()) {
+			openEditorWithFilePath();
+		}
+		else if (file != null) {
+			openEditorWithFile();
+		}
+		return Status.OK_STATUS;
+	}
+
+	/**
+	 * Opens the editor based on the given file path.
+	 * 
+	 * @since Creation date: 22.03.2012
+	 */
+	private void openEditorWithFilePath() {
+		IFile workspaceFile = OpenWorkspaceFileHelper.getWorkspaceFile(filePath);
+		if (workspaceFile != null) {
+			OpenWorkspaceFileHelper.openEditor(activePage, workspaceFile);
+		}
+		else {
+			OpenWorkspaceFileHelper.showAndLogErrorMessage("The resource could not be found in workspace.", null);
+		}
+	}
+
+	/**
+	 * Opens the editor with the given file.
+	 * 
+	 * @since Creation date: 22.03.2012
+	 */
+	private void openEditorWithFile() {
+		OpenWorkspaceFileHelper.openEditor(activePage, file);
+	}
+
+}
